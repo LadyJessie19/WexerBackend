8e6cb3bbc8f27e51b0d01e33d689f694b2978c1a
Jessie Moura <jessie.moura19@gmail.com>
2023-07-12 22:40:44 -0300

    chore: AuthModule creation

---
+++ src/app/Auth/AuthModule.ts
@@ -0,0 +1,13 @@
+import DoctorModule from "../Doctor/DoctorModule"
+import AuthController from "./AuthController"
+import AuthService from "./AuthService";
+
+class AuthModule {
+  static build() {
+    const service = new AuthService(DoctorModule.build().repository);
+    const controller = new AuthController(service);
+    return { service, controller }
+  }
+}
+
+export { AuthModule }
