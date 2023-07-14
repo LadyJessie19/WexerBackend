a5f7b1ef7536d73c45353784d19d83cabc8865c6
Jessie Moura <jessie.moura19@gmail.com>
2023-07-13 23:37:47 -0300

    fix: Auth Middleware was unfinished

--- src/middlewares/AuthenticateMiddleware/index.ts
+++ src/middlewares/AuthenticateMiddleware/index.ts
@@ -1,27 +1,32 @@
 import { NextFunction, Request, Response } from "express";
-import TokenHandler from "../../utils/TokenHandler";
-import newError from "../../utils/ErrorHandler";
 import dotenv from "dotenv"
+
+import newError from "../../utils/ErrorHandler";
+import TokenHandler from "../../utils/TokenHandler";
+import newSuccess from "../../utils/SuccessHandler";
+
 dotenv.config()
 
 class AuthenticateMiddleware{
-    static async checkToken(req: Request, res: Response, next: NextFunction) {
+    static async checkToken(req: Request | any, res: Response, next: NextFunction) {
         const { headers : { authorization } } = req
+        const secretKey = process.env.SECRET_KEY as string
+        const currentToken = new TokenHandler(secretKey)
         
         if(!authorization){
-            /* return res.status(401).json({error: 'Auth token is inaccessible'}) */
-            return res.status(401).json(newError('Auth token is inaccessible', 401))
+            return res.status(401).json(newError('Not authorized.', 401, "!authorization"))
         }
         
-        const [bear, token] = authorization.split(" ")
+        const [, token] = authorization.split(" ")
 
-        const currentToken = new TokenHandler(process.env.SECRET_KEY as string)
-
-        try{
-            currentToken.verify(token)
+        const isTheTokenValid = await currentToken.verify(token)
+        
+        if(isTheTokenValid){
+            req.user = await currentToken.decode(token)
+            return res.status(200).json(newSuccess("Auth completed.", 200, req.user))
             next()
-        }catch{
-            return res.status(401).json(newError('Not Authenticated', 401))
+        } else{
+            return res.status(401).json(newError('The token isn`t valid.', 401, "isTheTokenValid"))
         }
     }
 }
