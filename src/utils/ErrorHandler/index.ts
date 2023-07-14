826dc1aaed4c1092da8d159e406d0f937948dc59
Jessie Moura <jessie.moura19@gmail.com>
2023-07-13 23:32:34 -0300

    fix: adding an explanation to the ErrorHandler

--- src/utils/ErrorHandler/index.ts
+++ src/utils/ErrorHandler/index.ts
@@ -1,8 +1,16 @@
-const newError = (message:string, statusCode:number) => {
+/**
+ * This is a simple utils function. It spares code lines.
+ * @param message The request message. Nice to feedback the dev.
+ * @param statusCode The request status should be in here.
+ * @param flag This is to point were the error is coming from.
+ * @returns a response object.
+ */
+const newError = (message:string, statusCode:number, flag?:string | undefined) => {
     return {
         error: true,
         message,
         statusCode,
+        flag
       }
 }
 
