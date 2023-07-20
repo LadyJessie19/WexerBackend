1029c0fb7bc36a06481024390785bbd6c1de92fd
Jessie Moura <jessie.moura19@gmail.com>
2023-07-20 11:01:08 -0300

    chore: preparing the File Entity

--- src/routes/FileRoutes.ts
+++ src/routes/FileRoutes.ts
@@ -1,9 +1,31 @@
 import { Router } from "express";
 import FileModule from "../app/File/FileModule"
+import AuthenticateMiddleware from "../middlewares/AuthenticateMiddleware";
 
 const FileRoutes = Router()
 const fileController = FileModule.build().controller
 
-FileRoutes.post('/files', fileController.something.bind(fileController))
+//Create and getAll from a user;
+const occurrencePath = "/users/:user_id/files"
+//Create and getAll from a occurrences;
+const userPath = "/occurrences/:occurrence_id/files"
+//Get Them ALL!
+const commonPath = "/files"
+//GetOne, update and delete
+const pathWithId = "/files/:file_id"
+
+// FileRoutes.use(AuthenticateMiddleware.checkToken)
+
+//Create and read from a user
+FileRoutes.post(userPath, fileController.createCon.bind(fileController))
+FileRoutes.get(userPath, fileController.getFromParentCon.bind(fileController))
+//Create and read from a occurrence
+FileRoutes.post(occurrencePath, fileController.createCon.bind(fileController))
+FileRoutes.get(occurrencePath, fileController.getFromParentCon.bind(fileController))
+//Read
+FileRoutes.get(commonPath, fileController.getAllCon.bind(fileController))
+FileRoutes.get(pathWithId, fileController.getOneCon.bind(fileController))
+//Delete
+FileRoutes.delete(pathWithId, fileController.deleteCon.bind(fileController))
 
 export default FileRoutes
