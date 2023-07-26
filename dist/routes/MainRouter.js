ce5e74fddf1eb4b190625569813a8615163b466c
Isabelle_Moura <mourabisabelle@gmail.com>
2023-07-26 18:20:18 -0300

    build: MainRouter - auth middleware

--- dist/routes/MainRouter.js
+++ dist/routes/MainRouter.js
@@ -16,8 +16,8 @@ const MainRouter = (0, express_1.Router)();
 MainRouter.use(AuthRoutes_1.default);
 MainRouter.use(UserRoutes_1.default);
 /* Private Routes */
-MainRouter.use(AuthenticateMiddleware_1.default.checkToken);
 MainRouter.use(FileRoutes_1.default);
+MainRouter.use(AuthenticateMiddleware_1.default.checkToken);
 MainRouter.use(OccurrenceRoutes_1.default);
 MainRouter.use(PatientRoutes_1.default);
 MainRouter.use(TimelineRoutes_1.default);
